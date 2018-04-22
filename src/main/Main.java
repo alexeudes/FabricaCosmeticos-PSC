296bf6e2d5c47fc562c23a446ebcbee4d9e9fdb2
Diego Maylan <diego_maylan@hotmail.com>
2018-04-22 20:47:00 -0300

    Persistencia xml e main teste

---
+++ src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Main {
+
+	public static void main(String[] args) {
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("hibernatefc");
+	}
+
+}
