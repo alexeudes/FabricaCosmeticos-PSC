c74b3328cc293d21d72a51d5b258ef88a319b1d3
Edson <Edson@DESKTOP-TTVV84T>
2018-04-22 17:09:44 -0300

    atualiação cliente

--- src/basicas/Cliente.java
+++ src/basicas/Cliente.java
@@ -2,10 +2,14 @@ package basicas;
 
 import java.util.Collection;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+
 
 
 @Entity
@@ -14,19 +18,27 @@ public class Cliente {
 	//Atributos
 	@Id
 	@GeneratedValue	
-	private int id;
+	private Integer id;
+	@Column(length=14, nullable=false)
 	private String cnpj;
+	@Column(length=200, nullable=false)
 	private String razaoSocial;
+	@Column(length=11, nullable=false)
 	private String telefone;
+	@Column(length=40, nullable=false)
 	private String email;
+	
 	@OneToMany(mappedBy="Pedido")
-	private Collection <Pedido> pedido;	
+	private Collection <Pedido> pedido;
+	
+	@OneToOne
+	private Collection <Endereco> endereco;
 			
 	//Getters e Setters
-	public int getId() {
+	public Integer getId() {
 		return id;
 	}
-	public void setId(int id) {
+	public void setId(Integer id) {
 		this.id = id;
 	}
 	public String getCnpj() {
@@ -46,12 +58,23 @@ public class Cliente {
 	}
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
-	}
-	
+	}	
 	public String getEmail() {
 		return email;
 	}
 	public void setEmail(String email) {
 		this.email = email;
 	}	
+	public Collection<Pedido> getPedido() {
+		return pedido;
+	}
+	public void setPedido(Collection<Pedido> pedido) {
+		this.pedido = pedido;
+	}
+	public Collection<Endereco> getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(Collection<Endereco> endereco) {
+		this.endereco = endereco;
+	}
 }
