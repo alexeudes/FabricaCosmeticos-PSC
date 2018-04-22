bfc31769f2e94f9a643158f944bc8cfc5e9a5461
heitor santos <heitorsantos.esv@gmail.com>
2018-04-22 16:26:54 -0300

    classe endereco

---
+++ src/basicas/Endereco.java
@@ -0,0 +1,59 @@
+package basicas;
+
+import javax.persistence.*;
+
+public class Endereco {
+
+	//atributos
+	@Column(length = 9,nullable = false)
+	private String cep;
+	@Column(nullable= false)
+	private String logradouro;
+	@Column(nullable= false)
+	private String numero;
+	@Column(nullable= false)
+	private String bairro;
+	@Column(nullable= false)
+	private String cidade;
+	@Column(nullable= false)
+	private String estado;
+	
+	//Getters e Setters
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	public String getLogradouro() {
+		return logradouro;
+	}
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+		
+}
