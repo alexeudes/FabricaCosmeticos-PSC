597d38557839c95abc1920b8b85545823c8a8e10
Diego Maylan <diego_maylan@hotmail.com>
2018-04-22 18:07:07 -0300

    classe pessoa

--- src/basicas/Pessoa.java
+++ src/basicas/Pessoa.java
@@ -1,9 +1,74 @@
 package basicas;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy=InheritanceType.JOINED)
 public class Pessoa {
 
+	//Atributos
+	@Id
+	@GeneratedValue
+	public Integer id;
+	@Column (nullable = false, length = 11)
+	public String cpf;
+	@Column (nullable = false)
+	public String nome;
+	@Column (nullable = false, length = 11)
+	public String telefone;
+	@Column (nullable = false)
+	public String email;
+	@Column (nullable = false)
+	public String login;
+	@Column (nullable = false)
+	public String senha;
 	
-	public int id;
-	
-	
+	//Getters e Setters
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
 }
