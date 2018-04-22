911b050cec8492d81fa71c2a149aa4166528e763
Alex Eudes <alex.eudes@gmail.com>
2018-04-22 20:21:38 -0300

    Classes Teste e correção AtrasoObservacao

--- src/basicas/AtrasoObservacoes.java
+++ src/basicas/AtrasoObservacoes.java
@@ -2,10 +2,15 @@ package basicas;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class AtrasoObservacoes {
 
+	@Id
+	@GeneratedValue
+	private Integer id;
 	
 	//Atributos
 	@Column(nullable=false)
@@ -19,4 +24,14 @@ public class AtrasoObservacoes {
 	public void setObservacao(String observacao) {
 		this.observacao = observacao;
 	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	
 }
---
+++ src/basicas/Teste.java
@@ -0,0 +1,14 @@
+package basicas;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Teste {
+
+	public static void main(String[] args) {
+
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("hibernatefc");
+		factory.close();
+	}
+
+}
