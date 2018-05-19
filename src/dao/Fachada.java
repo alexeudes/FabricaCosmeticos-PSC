1b7d9bd2fe88e3658e1eaeb9b81a62a9867ba687
Diego Maylan <diego_maylan@hotmail.com>
2018-05-19 11:03:56 -0300

    fachada

---
+++ src/dao/Fachada.java
@@ -0,0 +1,18 @@
+package dao;
+
+public class Fachada {
+
+	private static Fachada instancia;
+	
+	private Fachada() {
+		super();
+	}
+	
+	public static Fachada getInstancia() {
+        if (instancia == null) {
+            instancia = new Fachada();
+        }
+        return instancia;
+	}
+	
+}
