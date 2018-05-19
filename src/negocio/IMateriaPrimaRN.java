e91c222c21bc13eb9f07b5b3c32180928e39069a
Diego Maylan <diego_maylan@hotmail.com>
2018-05-19 11:29:36 -0300

    metodos crud materia prima rn

--- src/negocio/IMateriaPrimaRN.java
+++ src/negocio/IMateriaPrimaRN.java
@@ -1,5 +1,18 @@
 package negocio;
 
-public interface IMateriaPrimaRN {
+import java.util.List;
 
+import basicas.MateriaPrima;
+
+public interface IMateriaPrimaRN {
+	
+	public List<MateriaPrima> getAll();
+	
+	public MateriaPrima searchByKey(Integer chave);
+	
+	public void insert(MateriaPrima materiaPrima);
+	
+	public MateriaPrima update(MateriaPrima materiaPrima);
+	
+	public void remove(MateriaPrima materiaPrima);
 }
--- src/negocio/MateriaPrimaRN.java
+++ src/negocio/MateriaPrimaRN.java
@@ -1,5 +1,37 @@
 package negocio;
 
-public class MateriaPrimaRN implements IMateriaPrimaRN {
+import java.util.List;
 
+import basicas.MateriaPrima;
+import dao.DAOFactory;
+import dao.MateriaPrimaDAO;
+
+public class MateriaPrimaRN implements IMateriaPrimaRN {
+	
+	private MateriaPrimaDAO materiaprimaDAO = DAOFactory.getMateriaPrimaDAO();
+
+	@Override
+	public List<MateriaPrima> getAll() {
+		return materiaprimaDAO.getAll();
+	}
+	
+	@Override
+	public MateriaPrima searchByKey(Integer chave) {
+		return materiaprimaDAO.searchByKey(chave);
+	}
+
+	@Override
+	public void insert(MateriaPrima materiaPrima) {
+		materiaprimaDAO.insert(materiaPrima);
+	}
+	
+	@Override
+	public MateriaPrima update(MateriaPrima materiaPrima) {
+		return materiaprimaDAO.update(materiaPrima);
+	}
+
+	@Override
+	public void remove(MateriaPrima materiaPrima) {
+		materiaprimaDAO.remove(materiaPrima);
+	}
 }
