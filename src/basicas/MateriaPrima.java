94479759e7a54f14572210368059fcd05d5c0452
Diego Maylan <diego_maylan@hotmail.com>
2018-06-14 06:32:19 -0300

    Adicionar botão voltar e corrigir fornecedor e validade na Materia Prima

--- src/basicas/MateriaPrima.java
+++ src/basicas/MateriaPrima.java
@@ -26,99 +26,111 @@ import org.hibernate.annotations.FetchMode;
 @Entity
 public class MateriaPrima {
 
-	//Atributos
+	// Atributos
 	@Id
-	@GeneratedValue	
+	@GeneratedValue
 	private Integer id;
-	
+
 	@Column(nullable = false)
 	@NotNull
 	private String nome;
-	
+
 	@Column(nullable = false)
 	@NotNull
 	private String descricao;
-	
+
 	@Column(nullable = false)
 	@NotNull
 	private String lote;
-	
+
 	@Temporal(TemporalType.DATE)
 	private Date validade;
-	
+
 	@Column(nullable = false)
 	@NotNull
 	private Integer estoqueAtual;
-	
-	//Relacionamentos
-	@OneToMany(mappedBy="materiaPrima",fetch = FetchType.LAZY)
+
+	// Relacionamentos
+	@OneToMany(mappedBy = "materiaPrima", fetch = FetchType.LAZY)
 	private Collection<Compra> compra;
-	
+
 	@ManyToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name="idFornecedor")
+	@JoinColumn(name = "idFornecedor")
 	private Fornecedor fornecedor;
-	
+
 	@ManyToMany(fetch = FetchType.LAZY)
-	@JoinTable(name="ProdutoMateriaPrima",
-				joinColumns = @JoinColumn(name="idMateriaPrima"),
-				inverseJoinColumns=@JoinColumn(name ="idProduto"))
+	@JoinTable(name = "ProdutoMateriaPrima", joinColumns = @JoinColumn(name = "idMateriaPrima"), inverseJoinColumns = @JoinColumn(name = "idProduto"))
 	private List<Produto> produto;
-	
-	//Constutor
+
+	// Constutor
 	public MateriaPrima() {
 		super();
 		this.fornecedor = new Fornecedor();
 	}
-	
-	//Getters e Setters
+
+	// Getters e Setters
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getDescricao() {
 		return descricao;
 	}
+
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+
 	public String getLote() {
 		return lote;
 	}
+
 	public void setLote(String lote) {
 		this.lote = lote;
 	}
+
 	public Date getValidade() {
 		return validade;
 	}
+
 	public void setValidade(Date validade) {
 		this.validade = validade;
 	}
+
 	public Integer getEstoqueAtual() {
 		return estoqueAtual;
 	}
+
 	public void setEstoqueAtual(Integer estoqueAtual) {
 		this.estoqueAtual = estoqueAtual;
 	}
+
 	public Fornecedor getFornecedor() {
 		return fornecedor;
 	}
+
 	public void setFornecedor(Fornecedor fornecedor) {
 		this.fornecedor = fornecedor;
 	}
+
 	public List<Produto> getProduto() {
 		return produto;
 	}
+
 	public void setProduto(List<Produto> produto) {
 		this.produto = produto;
 	}
-	
-	
+
 }
--- src/beans/MateriaPrimaBean.java
+++ src/beans/MateriaPrimaBean.java
@@ -16,7 +16,7 @@ import negocio.Fachada;
 @ManagedBean
 public class MateriaPrimaBean {
 
-	//Atributos
+	// Atributos
 	private Integer id;
 	private String nome;
 	private String descricao;
@@ -25,66 +25,81 @@ public class MateriaPrimaBean {
 	private Integer estoqueAtual;
 	private Collection<Compra> compra;
 	private Fornecedor fornecedor;
-	
+
 	private MateriaPrima materiaPrima;
 	private Collection<Fornecedor> fornecedores;
 	private List<MateriaPrima> materiasPrimas;
-	
+
 	private Fachada fachada = Fachada.getInstancia();
-	
-	//Construtores
+
+	// Construtores
 	public MateriaPrimaBean() {
 		this.compra = new ArrayList<Compra>();
 		this.fornecedor = new Fornecedor();
-		this.materiaPrima = new MateriaPrima(); 
+		this.materiaPrima = new MateriaPrima();
 	}
-	
-	//Getters e Setters
+
+	// Getters e Setters
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getDescricao() {
 		return descricao;
 	}
+
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+
 	public String getLote() {
 		return lote;
 	}
+
 	public void setLote(String lote) {
 		this.lote = lote;
 	}
+
 	public Date getValidade() {
 		return validade;
 	}
+
 	public void setValidade(Date validade) {
 		this.validade = validade;
 	}
+
 	public Integer getEstoqueAtual() {
 		return estoqueAtual;
 	}
+
 	public void setEstoqueAtual(Integer estoqueAtual) {
 		this.estoqueAtual = estoqueAtual;
 	}
+
 	public Collection<Compra> getCompra() {
 		return compra;
 	}
+
 	public void setCompra(Collection<Compra> compra) {
 		this.compra = compra;
 	}
+
 	public Fornecedor getFornecedor() {
 		return fornecedor;
 	}
+
 	public void setFornecedor(Fornecedor fornecedor) {
 		this.fornecedor = fornecedor;
 	}
@@ -92,37 +107,42 @@ public class MateriaPrimaBean {
 	public MateriaPrima getMateriaPrima() {
 		return materiaPrima;
 	}
+
 	public void setMateriaPrima(MateriaPrima materiaPrima) {
 		this.materiaPrima = materiaPrima;
 	}
+
 	public Collection<Fornecedor> getFornecedores() {
 		return fornecedores;
 	}
+
 	public void setFornecedores(Collection<Fornecedor> fornecedores) {
 		this.fornecedores = fornecedores;
 	}
+
 	public List<MateriaPrima> getMateriasPrimas() {
 		return materiasPrimas;
 	}
+
 	public void setMateriasPrimas(List<MateriaPrima> materiasPrimas) {
 		this.materiasPrimas = materiasPrimas;
 	}
 
-	//Método que carrega uma lista de Matéria prima ao inicializar a tela
+	// Método que carrega uma lista de Matéria prima ao inicializar a tela
 	@PostConstruct
 	public void init() {
-		this.setFornecedores(new ArrayList<Fornecedor>());
-		this.setMateriasPrimas(new ArrayList<MateriaPrima>());
+		this.fornecedores = new ArrayList<Fornecedor>();
+		this.materiasPrimas = new ArrayList<MateriaPrima>();
 		this.fornecedores = fachada.getAllFornecedor();
 		this.materiasPrimas = fachada.getAllMateriaPrima();
 	}
-	
-	//Método para inserir a Matéria-Prima
+
+	// Método para inserir a Matéria-Prima
 	public void insert() {
 		this.fachada.insertMateriaPrima(this.materiaPrima);
 	}
-	
-	public void delete(MateriaPrima mp) { 
+
+	public void delete(MateriaPrima mp) {
 		this.fachada.removeMateriaPrima(mp);
 	}
 }
